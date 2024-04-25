ef0ae130b702db9cf0b67735e80d77e7b89105b6
amaan8429 <mohdamaan8429@gmail.com>
2024-04-25 22:34:19 +0530

    admin thing corrected

---
+++ README.md
@@ -0,0 +1,74 @@
+# Udemy Clone Project - Todo List
+
+## Project Description
+
+This project is a clone of Udemy, developed using Next.js and Tailwind CSS.
+
+## Features to add
+
+### 1. Navbar
+
+- [ ] Implement Twitter-style CSS
+- [ ] Ensure hydration for better performance
+
+### 2. Data Fetching
+
+- [ ] Integrate SWR for improved data fetching
+
+### 3. App Structure Refinement
+
+- [ ] Refactor and finalize app structure
+- [ ] Ensure code modularity and scalability
+
+### 4. Data Validation
+
+- [ ] Incorporate Zod for robust data validation
+
+### 5. Session Management
+
+- [ ] Resolve session-related issues for smoother user experience
+
+### 6. Loading Optimization
+
+- [ ] Add loader component for enhanced user feedback
+- [ ] Implement setTimeout for loader to improve user experience
+
+### 7. Admin Panel
+
+- [ ] Develop UI for adding courses
+- [ ] Implement admin functionalities
+
+### 8. Email Integration
+
+- [ ] Setup email functionalities
+  - [ ] Course addition notification
+  - [ ] Purchase course confirmation
+  - [ ] Account creation verification
+
+### 9. Payment Gateway
+
+- [ ] Integrate Stripe for secure payment processing
+
+### 10. Video Calling Feature
+
+- [ ] Implement Webrtc call functionality for admin interactions
+
+### 11. Chatbot Integration
+
+- [ ] Integrate chatbot (Deepseek or Gemini) for enhanced user support
+
+### 12. Social Login
+
+- [ ] Implement Google and GitHub login functionalities for ease of access
+
+### 13. Notification System
+
+- [ ] Add toaster notifications for important events and actions
+
+### 14. Interface Enhancement
+
+- [ ] Resolve interface issues for next-auth for better user experience
+
+## Contributing
+
+Contributions are welcome! Feel free to fork the repository and submit pull requests to help improve the project. You can also DM me on [Twitter](https://x.com/amaan8429) to collaborate on this project or future projects.
---
+++ src/components/ui/demo/hero_parelllax.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+
+export function HeroParallaxDemo() {
+  return <HeroParallax products={products} />;
+}
+export const products = [
+  {
+    title: "Open Source",
+    link: "https://javascriptmastery.com",
+    thumbnail: "https://i3.ytimg.com/vi/o2CSJYXJBpc/hqdefault.jpg",
+  },
+  {
+    title: "Build Vercel Clone",
+    link: "https://cursor.so",
+    thumbnail: "https://img.youtube.com/vi/c8_tafixiAs/mqdefault.jpg",
+  },
+  {
+    title: "Next.js Mastery",
+    link: "https://userogue.com",
+    thumbnail: "https://img.youtube.com/vi/wm5gMKuwSYk/mqdefault.jpg",
+  },
+  {
+    title: "Open Source",
+    link: "https://javascriptmastery.com",
+    thumbnail: "https://i3.ytimg.com/vi/o2CSJYXJBpc/hqdefault.jpg",
+  },
+  {
+    title: "Build Vercel Clone",
+    link: "https://cursor.so",
+    thumbnail: "https://img.youtube.com/vi/c8_tafixiAs/mqdefault.jpg",
+  },
+  {
+    title: "Next.js Mastery",
+    link: "https://userogue.com",
+    thumbnail: "https://img.youtube.com/vi/wm5gMKuwSYk/mqdefault.jpg",
+  },
+
+  {
+    title: "Open Source",
+    link: "https://javascriptmastery.com",
+    thumbnail: "https://i3.ytimg.com/vi/o2CSJYXJBpc/hqdefault.jpg",
+  },
+  {
+    title: "Build Vercel Clone",
+    link: "https://cursor.so",
+    thumbnail: "https://img.youtube.com/vi/c8_tafixiAs/mqdefault.jpg",
+  },
+  {
+    title: "Next.js Mastery",
+    link: "https://userogue.com",
+    thumbnail: "https://img.youtube.com/vi/wm5gMKuwSYk/mqdefault.jpg",
+  },
+  {
+    title: "Open Source",
+    link: "https://javascriptmastery.com",
+    thumbnail: "https://i3.ytimg.com/vi/o2CSJYXJBpc/hqdefault.jpg",
+  },
+  {
+    title: "Build Vercel Clone",
+    link: "https://cursor.so",
+    thumbnail: "https://img.youtube.com/vi/c8_tafixiAs/mqdefault.jpg",
+  },
+  {
+    title: "Next.js Mastery",
+    link: "https://userogue.com",
+    thumbnail: "https://img.youtube.com/vi/wm5gMKuwSYk/mqdefault.jpg",
+  },
+  {
+    title: "Open Source",
+    link: "https://javascriptmastery.com",
+    thumbnail: "https://i3.ytimg.com/vi/o2CSJYXJBpc/hqdefault.jpg",
+  },
+  {
+    title: "Build Vercel Clone",
+    link: "https://cursor.so",
+    thumbnail: "https://img.youtube.com/vi/c8_tafixiAs/mqdefault.jpg",
+  },
+  {
+    title: "Next.js Mastery",
+    link: "https://userogue.com",
+    thumbnail: "https://img.youtube.com/vi/wm5gMKuwSYk/mqdefault.jpg",
+  },
+];
